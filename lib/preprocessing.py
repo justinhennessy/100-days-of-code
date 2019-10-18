6379a3c80bb4cb6ee62dd11183cbd8bd00cdf7a9
Justin Hennessy <justin@neto.com.au>
2019-10-19 05:54:06 +1000

    Made a bunch of changes to preprocessing, added new methods to clean up, adding features for changed columns

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -22,11 +22,13 @@ def currency_tonumeric(dataframe,feature):
     # Convert from a string to a float
     dataframe[feature] = pd.to_numeric(dataframe[feature].str.replace(',', ''))
 
-def default_nan(dataframe, features_array):
+def default_nan(df, features_array):
     # Default NaN values with median
     for feature in features_array:
-        default_value = dataframe[feature].fillna(dataframe[feature].median())
-        dataframe[feature] = dataframe[feature].fillna(default_value).astype(int)
+        default_value = df[feature].fillna(df[feature].median())
+        df[feature] = df[feature].fillna(default_value).astype(int)
+        df[feature + 'was_nan'] = 1
+    return df
 
 def default_annual_revenue(dataframe):
     # Fix missing values for annual revenue, replace with mean/trimmed mean of the plan size they are on
@@ -39,51 +41,54 @@ def default_annual_revenue(dataframe):
         trimmed_mean = trim_mean(dataframe.annual_revenue[dataframe.plan == plan].values, 0.1)
 
         if pd.isnull(mean):
-            revenue = 0
+            revenue = platform_revenue_median
         else:
             revenue = mean
-        dataframe.loc[dataframe.plan==plan, 'annual_revenue'] = dataframe.loc[dataframe.plan==plan, 'annual_revenue'].fillna(revenue)
-
-    dataframe['annual_revenue'][dataframe['annual_revenue'] == 0] = dataframe['annual_revenue'].median()
+        dataframe.loc[dataframe.plan==plan, 'annual_revenue'] = dataframe.loc[dataframe.plan == plan, 'annual_revenue'].fillna(revenue)
+        dataframe['annual_revenue_was_missing'] = 1
 
 def features_with_nan(df):
     for feature in df.columns:
         if len(df[df[feature].isna() == True]) > 0:
             print('Feature has NaN values: ' + feature)
 
-def prepare_data(df_raw):
-    # Sort data by date
-    df_raw = df_raw.sort_values(by='licence_registration_date')
+def preprocess_nps(df):
+    # Set default values for NaN values in NPS
+    df.nps = df.nps.fillna(-1)
 
-    # Convert annual_revenue from a string to a float
-    currency_tonumeric(df_raw, 'annual_revenue')
+    # 'bin' nps
+    # Issue with this is NaN gets given a detractor status which isn't right
+    bins = [-2, 0, 6.9, 8.9, 10]
+    group_names = ['no_data', 'detractor', 'passive', 'promoter']
 
-    # Convert fields to INT and setting any NaNs to the mean of that type
-    case_types = ['cases_total','cases_open','cases_closed','cases_age_hours_total','cases_age_hours_average', 'last_login_days']
+    df['nps'] = pd.cut(df['nps'], bins, labels=group_names)
+    return df
 
-    default_nan(df_raw, case_types)
-
-    default_annual_revenue(df_raw)
-
+def preprocess_last_login_days(df):
     # 'bin' last login days
-    #bins = [1, 3, 7, 14, 30, 60]
-    #group_names = ['day', 'few_days', 'week', 'fortnight', 'month']
+    bins = [1, 3, 7, 14, 30, 60]
+    group_names = ['day', 'few_days', 'week', 'fortnight', 'month']
 
     # need to get the mean of the plan size for last_login_days and set each row to that
     #df_raw.last_login_days = df_raw.last_login_days.fillna(np.mean(df_raw.last_login_days))
 
-    #last_login_categories = pd.cut(df_raw['last_login_days'], bins, labels=group_names)
-    #df_raw['last_login_categories'] = pd.cut(df_raw['last_login_days'], bins, labels=group_names)
+    df['last_login_categories'] = pd.cut(df['last_login_days'], bins, labels=group_names)
+    return df
 
-    # Set default values for NaN values in NPS
-    df_raw.nps = df_raw.nps.fillna(np.nanmean(df_raw.nps))
+def prepare_data(df_raw):
+    df_raw = df_raw.sort_values(by='licence_registration_date')
 
-    # 'bin' nps
-    # Issue with this is NaN gets given a detractor status which isn't right
-    bins = [0,6.9,8.9,10]
-    group_names = ['detractor', 'passive', 'promoter']
+    currency_tonumeric(df_raw, 'annual_revenue')
 
-    df_raw['nps'] = pd.cut(df_raw['nps'], bins, labels=group_names)
+    df_raw = default_nan(df_raw, ['cases_total', 'cases_open', 'cases_closed',
+                                  'cases_age_hours_total', 'cases_age_hours_average',
+                                  'last_login_days'])
+
+    default_annual_revenue(df_raw)
+
+    df_raw = preprocess_last_login_days(df_raw)
+
+    df_raw = preprocess_nps(df_raw)
 
     # one-hot encode fields
     #dummy_columns = ['customer_account_status', 'last_login_categories', 'plan', 'nps']
@@ -100,21 +105,23 @@ def prepare_data(df_raw):
     add_datepart(df_raw, 'licence_registration_date')
     add_datepart(df_raw, 'golive_date')
 
-    for feature in ['days_active', 'golive_days', 'cases_age_hours_total', 'annual_revenue']:
-        df_raw = logify_feature(df_raw, feature)
+    # Disabled because we dont necissarily need normal distribution for Random Forest models
+    #for feature in ['days_active', 'golive_days', 'cases_age_hours_total', 'annual_revenue']:
+    #    df_raw = logify_feature(df_raw, feature)
 
     # Drop columns, some of these create "Data Leakage", some are just to test if it has impact when they are taken out
     df_raw = df_raw.drop(columns=['customer_account_status_Good', 'last_login_concern',
                                   'last_login_days', 'account_status', 'changing_platform',
                                   'new_platform', 'licence_status', 'canceldate',
                                   'cancel_details', 'cancel_reason', 'url', 'merchant',
-                                  'total_churn_concern_cases_age'])
+                                  'total_churn_concern_cases_age', 'username'])
 
     # Set any remaining NaNs to the features median
     for feature in df_raw.select_dtypes(include=['float64', 'int64']).columns:
         median = df_raw[feature].median()
         #print(f"{feature}: {median}")
         df_raw[feature] = df_raw[feature].fillna(df_raw[feature].median())
+        df_raw[feature + '_was_nan'] = 1
 
     # Complete the transformation of all data into
     # numbers using proc_df and create training dataframes
