1eefaf991a3342559533255974628e6b99bd4146
Justin Hennessy <justin@neto.com.au>
2019-10-23 20:30:34 +1000

    Fixed rows with no plan when defaulting annual_revenue, updated drop feature list to include interations

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -52,30 +52,49 @@ def default_nan(df, features_array):
     for feature in features_array:
         default_value = df[feature].fillna(df[feature].median())
         df[feature] = df[feature].fillna(default_value).astype(int)
-        df[feature + 'was_nan'] = 1
+        df[feature + '_was_nan'] = 1
     return df
 
-def default_annual_revenue(dataframe):
+def default_annual_revenue(df):
     # Fix missing values for annual revenue, replace with mean/trimmed mean of the plan size they are on
-    plan_list = dataframe.plan[~pd.isnull(dataframe.plan)].unique()
 
-    platform_revenue_median = dataframe.annual_revenue.median()
+    platform_revenue_median = df.annual_revenue.median()
+    print(f" Platform revenue median: {platform_revenue_median}")
+
+    df['plan_not_set'] = 0
+
+    for i in df[df.plan.isna() == True].index:
+        df['plan'][i] = "no_plan"
+        df['plan_not_set'][i] = 1
+
+    plan_list = df.plan[~pd.isnull(df.plan)].unique()
 
     for plan in plan_list:
-        mean = round(dataframe.annual_revenue[dataframe.plan == plan].mean(), 2)
-        trimmed_mean = trim_mean(dataframe.annual_revenue[dataframe.plan == plan].values, 0.1)
+        mean = abs(round(df.annual_revenue[df.plan == plan].mean(), 2))
+        trimmed_mean = trim_mean(df.annual_revenue[df.plan == plan].values, 0.1)
 
         if pd.isnull(mean):
             revenue = platform_revenue_median
         else:
             revenue = mean
-        dataframe.loc[dataframe.plan==plan, 'annual_revenue'] = dataframe.loc[dataframe.plan == plan, 'annual_revenue'].fillna(revenue)
-        dataframe['annual_revenue_was_missing'] = 1
+        df.loc[df.plan == plan, 'annual_revenue'] = \
+            df.loc[df.plan == plan, 'annual_revenue'].fillna(revenue)
+        df['annual_revenue_was_missing'] = 1
+
+    if len(df[df.plan.isna() == True]) > 0:
+      print("Output customers with no plan set ...")
+      print(df.username[df.plan.isna() == True])
+
+    if len(df[df['annual_revenue'].isna() == True]) > 0:
+        print("Output records with a zero revenue ...")
+        print(df.username[df['annual_revenue'].isna() == True])
+
+    return df
 
 def features_with_nan(df):
     for feature in df.columns:
         if len(df[df[feature].isna() == True]) > 0:
-            print('Feature has NaN values: ' + feature)
+            print(f"Feature has NaN values: {feature}, {len(df[df[feature].isna() == True])}")
 
 def preprocess_nps(df):
     # Set default values for NaN values in NPS
@@ -114,7 +133,7 @@ def prepare_data(df_raw):
                                   'last_login_days'])
 
     print("Defaulting annual_revenue for missing values to the plans mean or platform medeian ...")
-    default_annual_revenue(df_raw)
+    df_raw = default_annual_revenue(df_raw)
 
     print("Binning last_login_days")
     df_raw = preprocess_last_login_days(df_raw)
@@ -144,20 +163,24 @@ def prepare_data(df_raw):
     #for feature in ['days_active', 'golive_days', 'cases_age_hours_total', 'annual_revenue']:
     #    df_raw = logify_feature(df_raw, feature)
 
-    print("Dropping features ...")
     # Drop columns, some of these create "Data Leakage", some are just to test if it has impact when they are taken out
-    df_raw = df_raw.drop(columns=['customer_account_status_Good', 'last_login_concern',
-                                  'last_login_days', 'account_status', 'changing_platform',
-                                  'new_platform', 'licence_status', 'canceldate',
-                                  'cancel_details', 'cancel_reason', 'url', 'merchant',
-                                  'total_churn_concern_cases_age'])
+    for feature in ['customer_account_status_Good', 'last_login_concern', 'last_login_days', 'account_status', 'changing_platform',
+                                  'new_platform', 'licence_status', 'canceldate', \
+                                  'cancel_details', 'cancel_reason', 'url', 'merchant', \
+                                  'total_churn_concern_cases_age']:
+        print(f"Dropping feature {feature} ...")
+        df_raw = df_raw.drop(columns=[feature])
 
-    print("Replacing NaN values with median and adding '_was_nan' column ...")
+    for regex_filter in ['interactions', 'churn_concern']:
+        print(f"Dropping features with '{regex_filter}' in their name ...")
+        df_raw = df_raw[df_raw.columns.drop(list(df_raw.filter(regex=regex_filter)))]
+
+    #print("Replacing NaN values with median and adding '_was_nan' column ...")
     # Set any remaining NaNs to the features median
-    for feature in df_raw.select_dtypes(include=['float64', 'int64']).columns:
-        median = df_raw[feature].median()
-        df_raw[feature] = df_raw[feature].fillna(df_raw[feature].median())
-        df_raw[feature + '_was_nan'] = 1
+    #for feature in df_raw.select_dtypes(include=['float64', 'int64']).columns:
+    #    median = df_raw[feature].median()
+    #    df_raw[feature] = df_raw[feature].fillna(df_raw[feature].median())
+    #    df_raw[feature + '_was_nan'] = 1
 
     print("Convert categorical features into numbers ...")
     # Complete the transformation of all data into
